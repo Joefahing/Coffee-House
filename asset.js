dddc42ddbeb61120f3fa76be1ddacf518a42790a
joefahing <cao11d@uwindsor.ca>
2021-07-10 14:57:53 -0400

    initial commit

---
+++ asset.js
@@ -0,0 +1,26 @@
+const style = {
+    color:{
+        primary:{
+            brand: '#ab6b51',
+            brand_light: '#d0b49f',
+            cta: '#39918c',
+            white: '#fff',
+            black: '#191919',
+            grayscale300: '#e3e3e3',
+            grayscale400: '#d2d2d2',
+            grayscale600: '#a3a3a3',
+            grayscale800: '#7d7d7d',
+            link: '#1755b7'
+        }
+    },
+    font:{
+        main_font: 'https://fonts.googleapis.com/css2?family=Nunito:wght@200;300;400;600;700&display=swap',
+        alternative_font: 'Monaco'
+    }
+}
+
+
+
+
+
+
